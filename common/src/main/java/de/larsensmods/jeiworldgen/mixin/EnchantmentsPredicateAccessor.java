cd29971c823c1995b215dad6d8b8c8a1d274bf14
Larsen Rektor <larsen@lrektor.de>
2026-06-19 16:37:50 +0200

    Updated to Minecraft 26.2

--- common/src/main/java/de/larsensmods/jeiworldgen/mixin/EnchantmentsPredicateAccessor.java
+++ common/src/main/java/de/larsensmods/jeiworldgen/mixin/EnchantmentsPredicateAccessor.java
@@ -1,6 +1,6 @@
 package de.larsensmods.jeiworldgen.mixin;
 
-import net.minecraft.advancements.criterion.EnchantmentPredicate;
+import net.minecraft.advancements.predicates.EnchantmentPredicate;
 import net.minecraft.core.component.predicates.EnchantmentsPredicate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
