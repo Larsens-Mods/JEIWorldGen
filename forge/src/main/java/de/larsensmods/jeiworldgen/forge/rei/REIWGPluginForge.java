4176d2ad1843b35ac4782b02de8dada8d4ff5627
Larsen Rektor <larsen@lrektor.de>
2026-06-29 00:08:19 +0200

    Added REI Compatibility

---
+++ forge/src/main/java/de/larsensmods/jeiworldgen/forge/rei/REIWGPluginForge.java
@@ -0,0 +1,8 @@
+package de.larsensmods.jeiworldgen.forge.rei;
+
+import de.larsensmods.jeiworldgen.rei.REIWGPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
+
+@REIPluginClient
+public class REIWGPluginForge extends REIWGPlugin {
+}
