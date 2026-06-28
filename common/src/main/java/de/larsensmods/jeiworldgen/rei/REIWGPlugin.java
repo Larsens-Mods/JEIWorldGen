761c048375281d2bb69218f8d056cf40d9040b31
Larsen Rektor <larsen@lrektor.de>
2026-06-28 19:59:23 +0200

    Added REI support
    (cherry picked from commit 76f91b98516335154ef75c3889e65338285cb105)

---
+++ common/src/main/java/de/larsensmods/jeiworldgen/rei/REIWGPlugin.java
@@ -0,0 +1,22 @@
+package de.larsensmods.jeiworldgen.rei;
+
+import de.larsensmods.jeiworldgen.events.ClientEvents;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import net.minecraft.client.Minecraft;
+
+public class REIWGPlugin implements REIClientPlugin {
+
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new WorldGenCategory());
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        ClientEvents.playerJoinedWorld(Minecraft.getInstance().player);
+        WorldGenTypeHelper.buildRecipes().forEach(registry::add);
+    }
+
+}
